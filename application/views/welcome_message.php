3e71add3b8e0895fc63c29fa157595e0cdbc4b71
LeyWis1522 <ashmarc1522@gmail.com>
2022-11-06 21:10:05 +0800

    luismarc

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to Phasess</title>
+	<title>Welcome to Phasesss</title>
 
 	<style type="text/css">
 
