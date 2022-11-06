52cb0d32502080f9699d6b7f1dca83f0cd8ef3f5
AlanaMTC <100965248+AlanaMTC@users.noreply.github.com>
2022-11-06 20:36:30 +0800

    alana

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniterssssssssssssssssssssssssssssssss</title>
+	<title>Welcome to CodeIgnitersss</title>
 
 	<style type="text/css">
 
